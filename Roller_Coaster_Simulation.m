01b83c7486f2cf72f9419f9f48e7a4b77bcb1422
hensty11 <tyhensley2005@gmail.com>
2025-01-28 11:35:28 -0700

    Initial File Commit

---
+++ Roller_Coaster_Simulation.m
@@ -0,0 +1,28 @@
+%% ASEN 2803 Dynamics Lab 1 Simulation
+clc;
+clear;
+
+%% Initial Values
+x0 = 0; % initial x value (meters)
+y0 = 0; % initial y value (meters)
+z0 = 125; % initial z value (meters)
+v0 = 0; % initial velocity (m/s)
+
+% max G forces
+max_forward = 5; % back of seat
+max_back = 4; % bar toward back
+max_up = 6; % up through seat
+max_down = 1; % down from bar
+max_lat = 3; % left or right of rider
+
+max_length = 1250; % meters
+position = zeros(max_length,3); % matrix of positions over time
+position(1,:) = [x0 y0 z0];
+
+%% Zero G Parabola
+
+
+%% Loop
+
+
+%% Banked Turn
